04595ca750fd227d373386732c8f7bb68a02096e
ぎょくて <gyokutech@gmail.com>
2025-03-23 22:38:38 +0900

    fix: PlayerVoice.cs owner権限
    - 状態設定変化時、同時にplayerの音声を再設定するよう変更
    - 動作状況デバッグのため、[SerializeField]追加

--- Packages/jp.virtualaviation.udon-radio-communications-redux/Runtime/core/PlayerVoice.cs
+++ Packages/jp.virtualaviation.udon-radio-communications-redux/Runtime/core/PlayerVoice.cs
@@ -13,38 +13,43 @@ namespace UdonRadioCommunicationRedux
     {
         public UdonRadioCommunication urc;
 
-        private int currentSelectedProtocol = 0;
-        private bool[] enableProtocol;
-        private float[] protocolGainSetting;
-        private float[] protocolNearSetting;
-        private float[] protocolFarSetting;
-        private float[] protocolVolumetricRadiusSetting;
-        private bool[] protocolLowpassSetting;
+        [SerializeField] private int currentSelectedProtocol = 0;
+        [SerializeField] private bool[] enableProtocol;
+        [SerializeField] private float[] protocolGainSetting;
+        [SerializeField] private float[] protocolNearSetting;
+        [SerializeField] private float[] protocolFarSetting;
+        [SerializeField] private float[] protocolVolumetricRadiusSetting;
+        [SerializeField] private bool[] protocolLowpassSetting;
 
         private VRCPlayerApi owner;
-        private int ownerPlayerId;
+        [SerializeField] int ownerPlayerId;
 
 
         #region Voice Setting
         public void SetVoiceGain(int protocolPriority, float gain)
         {
             protocolGainSetting[protocolPriority] = gain;
+            if (currentSelectedProtocol == protocolPriority) owner.SetVoiceGain(gain);
         }
         public void SetVoiceNear(int protocolPriority, float near)
         {
             protocolNearSetting[protocolPriority] = near;
+            if (currentSelectedProtocol == protocolPriority) owner.SetVoiceDistanceNear(near);
         }
         public void SetVoiceFar(int protocolPriority, float far)
         {
             protocolFarSetting[protocolPriority] = far;
+            if (currentSelectedProtocol == protocolPriority) owner.SetVoiceDistanceFar(far);
         }
         public void SetVoiceVolumetricRadius(int protocolPriority, float radius)
         {
             protocolVolumetricRadiusSetting[protocolPriority] = radius;
+            if (currentSelectedProtocol == protocolPriority) owner.SetVoiceVolumetricRadius(radius);
         }
         public void SetVoiceLowpass(int protocolPriority, bool enabled)
         {
             protocolLowpassSetting[protocolPriority] = enabled;
+            if (currentSelectedProtocol == protocolPriority) owner.SetVoiceLowpass(enabled);
         }
 
         public void EnableVoiceProtocol(int protocolPriority)
