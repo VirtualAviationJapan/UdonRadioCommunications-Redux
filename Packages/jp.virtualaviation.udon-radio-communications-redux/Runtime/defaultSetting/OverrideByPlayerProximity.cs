c4fb9555dc767293830c8add21aee421d60e86c5
ぎょくて <gyokutech@gmail.com>
2025-05-24 13:27:43 +0900

    fix:OverrideByPlayerProximity
    - スポーン直後に設定がなされない問題を修正

--- Packages/jp.virtualaviation.udon-radio-communications-redux/Runtime/defaultSetting/OverrideByPlayerProximity.cs
+++ Packages/jp.virtualaviation.udon-radio-communications-redux/Runtime/defaultSetting/OverrideByPlayerProximity.cs
@@ -14,11 +14,12 @@ namespace UdonRadioCommunicationRedux
     {
         public DefaultSetting defaultSetting;
         private VRCPlayerApi localPlayer;
+        private float applyingDistance = 5;
+        private SphereCollider sc;
 
         public override void OnPlayerJoined(VRCPlayerApi player)
-
         {
-            if (player.isLocal) localPlayer = player;
+            if (player.isLocal) { localPlayer = player; }
         }
         public override void OnPlayerRespawn(VRCPlayerApi player)
         {
@@ -28,12 +29,18 @@ namespace UdonRadioCommunicationRedux
                 VRCPlayerApi.GetPlayers(players);
                 foreach (var p in players)
                 {
-                    urc.DisableVoiceProtocol(p.playerId, protocolPriority);
+                    if (CheckProximity(p))
+                        urc.EnableVoiceProtocol(p.playerId, protocolPriority);
+                    else
+                        urc.DisableVoiceProtocol(p.playerId, protocolPriority);
                 }
             }
             else
             {
-                urc.DisableVoiceProtocol(player.playerId, protocolPriority);
+                if (CheckProximity(player))
+                    urc.EnableVoiceProtocol(player.playerId, protocolPriority);
+                else
+                    urc.DisableVoiceProtocol(player.playerId, protocolPriority);
             }
         }
 
@@ -41,6 +48,12 @@ namespace UdonRadioCommunicationRedux
         public override void OnPlayerVoiceAdded(int playerId)
         {
             defaultSetting.SetDefaultSetting(playerId, protocolPriority);
+            // 近くにプレイヤーがいる場合、有効化
+            VRCPlayerApi player = VRCPlayerApi.GetPlayerById(playerId);
+            if (CheckProximity(player))
+            {
+                urc.EnableVoiceProtocol(playerId, protocolPriority);
+            }
         }
 
         public override void OnPlayerTriggerEnter(VRCPlayerApi player)
@@ -53,6 +66,12 @@ namespace UdonRadioCommunicationRedux
             if (player.isLocal) return;
             urc.DisableVoiceProtocol(player.playerId, protocolPriority);
         }
+
+        void Start()
+        {
+            sc = (SphereCollider)gameObject.GetComponent<SphereCollider>();
+            if (sc) applyingDistance = sc.radius;
+        }
         void Update()
         {
             if (Utilities.IsValid(localPlayer) == true)
@@ -61,6 +80,22 @@ namespace UdonRadioCommunicationRedux
                 VRCPlayerApi.TrackingData origin = localPlayer.GetTrackingData(VRCPlayerApi.TrackingDataType.Origin);
                 transform.position = origin.position;
             }
+        }
+
+        /// <summary>
+        ///  特定のプレイヤーと自プレイヤーの距離を判定し、近接するならtrueを返す
+        /// </summary>
+        /// <param name="player">距測対象のプレイヤー</param>
+        /// <returns></returns>
+        private bool CheckProximity(VRCPlayerApi player)
+        {
+            if (Utilities.IsValid(player))
+            {
+                VRCPlayerApi.TrackingData origin = player.GetTrackingData(VRCPlayerApi.TrackingDataType.Origin);
+                float distance = Vector3.Distance(origin.position, transform.position);
+                if (distance <= applyingDistance) return true;
+            }
+            return false;
         }
     }
 }
